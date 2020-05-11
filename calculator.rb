b163850585cfb5fdecb8b7150ed4fd75bbcd4a2f
Leamsi Escribano <leamsii.e@gmail.com>
2020-05-11 00:19:10 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,4 +2,7 @@
 first_number = 100
 second_number = 2
 
-difference = 100 - 2
+sum = first_number + second_number
+difference = first_number - second_number
+product = first_number * second_number
+quotient = 
